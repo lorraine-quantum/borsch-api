588f0b6745909ac0f08ba308e25856a28cadcc1c
lorraine-quantum <quantumoffice010@gmail.com>
2024-03-07 17:01:35 +0100

    port 3002

--- src/app.js
+++ src/app.js
@@ -69,7 +69,7 @@ app.get('/', (req, res) => {
 app.use("/", adminAuthMiddleware, adminRoutes);
 
 
-const port = process.env.PORT || 3008;
+const port = process.env.PORT || 3002;
 //switch between local and cloud db
 
 const local = process.env.LOCAL_URI;
